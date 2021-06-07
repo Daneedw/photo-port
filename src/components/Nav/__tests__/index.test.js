14996863486acf1e32873e4233b8b418d5271291
Dane Edwards <Daneedw@gmail.com>
2021-06-07 10:10:03 -0500

    basic tests added for nave

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,6 +1,8 @@
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
+
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
@@ -16,4 +18,24 @@ describe('Nav component', () => {
       })
 
       
+  })
+
+  describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    // Arrange
+    const { getByLabelText } = render(<Nav />);
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+
+    // Assert  
+    })
+  }) 
+
+  describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      // Arrange
+      const { getByTestId } = render(<Nav />);
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+      // Assert
+    });
   })

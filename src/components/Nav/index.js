09878680dfbbaff17619aa5dd0725c9cb65701a5
Dane Edwards <Daneedw@gmail.com>
2021-06-07 11:56:32 -0500

    contact form added, need to add tests still

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,6 +5,8 @@ function Nav({
   categories = [],
   setCurrentCategory,
   currentCategory,
+  contactSelected,
+  setContactSelected
 }) {
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
@@ -20,23 +22,26 @@ function Nav({
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a data-testid="about" href="#about">
+            <a data-testid="about" href="#about"    onClick={() => {
+                         setContactSelected(false);
+                }}>
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+          <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+                currentCategory.name === category.name && !contactSelected && `navActive`
                 }`}
               key={category.name}
             >
               <span
                 onClick={() => {
                   setCurrentCategory(category)
+                  setContactSelected(false);
                 }}
               >
                 {capitalizeFirstLetter(category.name)}
